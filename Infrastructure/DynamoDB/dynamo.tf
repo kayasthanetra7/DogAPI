5302a2f29d9a2379f7fe94ecfe25b0b2b815da09
Netra Kayastha <netrakayastha@nexsystech.com>
2021-08-10 16:56:47 -0400

    Updating dyanmo template

--- Infrastructure/DynamoDB/dynamo.tf
+++ Infrastructure/DynamoDB/dynamo.tf
@@ -1,6 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.32"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
 resource "aws_dynamodb_table" "table"{
     name = "Dogs"
-    billing_mode = "Pay_Per_Request"
+    billing_mode = "PAY_PER_REQUEST"
     hash_key = "Breed"
     attribute {
       name = "Breed"
@@ -8,31 +21,31 @@ resource "aws_dynamodb_table" "table"{
     }
 }
 
-resource "aws iam policy" "policy" {
+resource "aws_iam_policy" "policy" {
   name = "dynamo_allow_read"
   description = "Dynamo DB read, write and delete"
-
   policy = <<EOT
-  {
+{
     "Version": "2012-10-17",
     "Statement": [
-      {
-        "Action":[
-          "dynamodb: BatchgetItem",
-          "dynamodb: BatchWriteItem",
-          "dynamodb: ConditionCheckItem",
-          "dynamodb: PutItem",
-          "dynamodb: DescribeTable",
-          "dynamodb: DeleteItem",
-          "dynamodb: GetItem",
-          "dynamodb: Scan",
-          "dynamodb: Query",
-          "dynamodb: UpdateItem",
-        ],
-        "Effect": "Allow",
-        "Resource": "arn:aws:dynamodb:*:113313345969:table/Dogs"
-      },
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb: Scan",
+                "dynamodb: BatchgetItem",
+                "dynamodb: PutItem",
+                "dynamodb: DescribeTable",
+                "dynamodb: BatchWriteItem",
+                "dynamodb: Query",
+                "dynamodb: ConditionCheckItem",
+                "dynamodb: GetItem",
+                "dynamodb: DeleteItem",
+                "dynamodb: UpdateItem"
+            ],
+            "Resource": "arn:aws:dynamodb:*:113313345969:table/Dogs"
+        }
     ]
-  }
+}
 EOT
 }
